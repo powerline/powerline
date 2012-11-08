99ded1d0c6558cb49a7bdbc24358282798bbf60a
Kim Silkebækken <kim.silkebaekken@gmail.com>
2012-11-08 12:44:46 +0100

    Add vim statusline example
    This is currently rendered with the terminal renderer, and is just
    a simple proof-of-concept of how vim statuslines can be defined with the
    Python API.

---
+++ powerline-vim-example.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''Powerline vim statusline example.
+'''
+
+from lib.core import Segment
+from lib.renderers import TerminalSegmentRenderer
+
+powerline = Segment([
+	Segment('NORMAL', 22, 148, attr=Segment.ATTR_BOLD),
+	Segment('⭠ develop', 247, 240),
+	Segment([
+		Segment(' ~/projects/powerline/lib/'),
+		Segment('core.py ', 231, attr=Segment.ATTR_BOLD),
+	], 250, 240, separate=False, padding=''),
+	Segment(),
+	Segment([
+		Segment('unix'),
+		Segment('utf-8'),
+		Segment('python'),
+		Segment(' 83%', 247, 240),
+		Segment([
+			Segment(' ⭡ ', 239),
+			Segment('23', attr=Segment.ATTR_BOLD),
+			Segment(':1 ', 244),
+		], 235, 252, separate=False, padding=''),
+	], 245, side='r'),
+], bg=236)
+
+print(powerline.render(TerminalSegmentRenderer))
