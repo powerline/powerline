cbf8d16ed88bdfb5f9699076a6f2a689984ad4c2
Kim Silkebækken <kim.silkebaekken@gmail.com>
2012-11-16 12:49:54 +0100

    Ignore empty segments that aren't filler segments

--- lib/core.py
+++ lib/core.py
@@ -15,8 +15,11 @@ class Powerline:
 
 	def __init__(self, segments):
 		'''Create a new Powerline.
+
+		Segments that have empty contents and aren't filler segments are
+		dropped from the segment array.
 		'''
-		self.segments = segments
+		self.segments = [segment for segment in segments if segment.contents or segment.filler]
 
 	def render(self, renderer, width=None):
 		'''Render all the segments with the specified renderer.
