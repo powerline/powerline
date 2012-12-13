cd41910fb3be46235ac46ae99b9a416bc9619dcd
Kim Silkebækken <kim.silkebaekken@gmail.com>
2012-12-13 15:43:05 +0100

    Add Sphinx docs skeleton

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+sys.path.insert(0, os.path.abspath('../powerline'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Powerline'
+copyright = u'Kim Silkebækken'
+version = 'dev'
+release = 'dev'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+html_theme = 'nature'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Powerline
+=========
+
+.. toctree::
+   :maxdepth: 3
+   :glob:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
