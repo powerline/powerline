7fb3df28b9fc9222e44ae48d8b2fd3c2a6b9fbfa
ZyX <kp-pav@ya.ru>
2013-01-03 00:21:56 +0400

    Change code to use vim_get_func('fnameescape')
    Previous version had problems with paths containing backslashes and/or
    double quotes.

--- powerline/ext/vim/__init__.py
+++ powerline/ext/vim/__init__.py
@@ -4,5 +4,8 @@
 def source_plugin():
 	import os
 	import vim
+	from bindings import vim_get_func
 
-	vim.command('source ' + vim.eval('fnameescape("' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'powerline.vim') + '")'))
+	fnameescape = vim_get_func('fnameescape')
+
+	vim.command('source ' + fnameescape(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'powerline.vim')))
