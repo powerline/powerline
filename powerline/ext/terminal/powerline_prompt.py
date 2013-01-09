d0d35cac345b842bce36dfd9531f859988e82d55
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-01-09 14:40:06 +0100

    Move terminal prompt script to extension directory

---
+++ powerline/ext/terminal/powerline_prompt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+'''Powerline terminal prompt example.
+'''
+try:
+	from powerline.core import Powerline
+except ImportError:
+	import os
+	import sys
+	sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+	from powerline.core import Powerline
+
+pl = Powerline('terminal')
+print(pl.renderer.render(None).encode('utf-8'))
