44fb24dee67ef9970174fed24e853e75169b45d5
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-01-09 14:29:01 +0100

    Fix padding issue with single-sided statuslines

--- powerline/renderer.py
+++ powerline/renderer.py
@@ -89,7 +89,7 @@ class Renderer(object):
 			compare_segment = next_segment if segment['side'] == 'left' else prev_segment
 
 			segment['rendered_raw'] = u''
-			outer_padding = ' ' if index == 0 or index == segments_len - 1 else ''
+			outer_padding = ' ' if index == 0 or (index == segments_len - 1 and segment['side'] == 'right') else ''
 			divider_type = 'soft' if compare_segment['highlight'][mode]['bg'] == segment['highlight'][mode]['bg'] else 'hard'
 			divider = theme.get_divider(segment['side'], divider_type)
 
