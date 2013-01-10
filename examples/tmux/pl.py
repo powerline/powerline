e99e33710e859be388d3b0e2287220f4870c7017
Liam Curry <liam@curry.name>
2013-01-10 13:55:18 -0500

    added right/left options to tmux script; enhanced tmux example config

--- examples/tmux/pl.py
+++ examples/tmux/pl.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 '''Powerline tmux statusline example.
 
 Run with `tmux -f tmux.conf`.
 '''
-
+import argparse
 import os
 import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from powerline.core import Powerline
 
-pl = Powerline('tmux')
-print(pl.renderer.render('n').encode('utf-8'))
+parser = argparse.ArgumentParser(description='powerline outputter')
+parser.add_argument('side', nargs='?', default='all',
+                    choices=('all', 'left', 'right'))
+parser.add_argument('--ext', default='tmux')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    pl = Powerline(args.ext)
+    segments = pl.renderer.get_theme().get_segments()
+    if args.side != 'all':
+        segments = [s for s in segments if s['side'] == args.side]
+    print(pl.renderer.render('n', segments=segments).encode('utf-8'))
