65e358dee20ea07286ddb13c56d207952b6b7fbd
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-01-19 17:01:01 +0100

    Move terminal and tmux segments into common module
    This should ideally be renamed to something else since it's not strictly
    an extension. A better module naming scheme could be something like:
    
    	powerline.segments.ext.{extension}
    	powerline.segments.common
    
    This would move all segments out of their respective extension
    directory, and might be a bit confusing. The ext directories may also
    need some changes to make this work properly.

--- powerline/ext/common/segments.py
+++ powerline/ext/common/segments.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
+import socket
 
+from powerline.lib.vcs import guess
 from powerline.lib import memoize
 
 # Weather condition code descriptions available at http://developer.yahoo.com/weather/#codes
@@ -16,6 +19,43 @@ weather_conditions_codes = {
 	u'☼': [32, 36],
 	u'☾': [31, 33],
 }
+
+
+def hostname():
+	if not os.environ.get('SSH_CLIENT'):
+		return None
+	return socket.gethostname()
+
+
+def user():
+	user = os.environ.get('USER')
+	euid = os.geteuid()
+	return {
+		'contents': user,
+		'highlight': 'user' if euid != 0 else ['superuser', 'user'],
+		}
+
+
+def branch():
+	repo = guess(os.path.abspath(os.getcwd()))
+	if repo:
+		return repo.branch()
+	return None
+
+
+def cwd(dir_shorten_len=None, dir_limit_depth=None):
+	cwd = os.getcwdu()
+	home = os.environ.get('HOME')
+	if home:
+		cwd = re.sub('^' + re.escape(home), '~', cwd, 1)
+	cwd_split = cwd.split(os.sep)
+	cwd_split_len = len(cwd_split)
+	if cwd_split_len > dir_limit_depth + 1:
+		del(cwd_split[0:-dir_limit_depth])
+		cwd_split.insert(0, u'⋯')
+	cwd = [i[0:dir_shorten_len] if dir_shorten_len and i else i for i in cwd_split[:-1]] + [cwd_split[-1]]
+	cwd = os.path.join(*cwd)
+	return cwd
 
 
 def date(format='%Y-%m-%d'):
