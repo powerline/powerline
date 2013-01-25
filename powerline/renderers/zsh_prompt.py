683fd9a955f48e0b627117b7a57ff8536d1ac61b
ZyX <kp-pav@ya.ru>
2013-01-25 22:25:11 +0400

    Zsh prompt security and other fixes
    Escape percent in zsh prompt
    Prevent some security issues (directory named `$(echo abc)` appeared as `abc`) 
        (`"$()"` → `'$()'`)
    Removed exports

--- powerline/renderers/zsh_prompt.py
+++ powerline/renderers/zsh_prompt.py
@@ -5,10 +5,14 @@ from powerline.renderers.shell import ShellRenderer
 
 class ZshPromptRenderer(ShellRenderer):
 	'''Powerline zsh prompt segment renderer.'''
-	def hl(self, fg=None, bg=None, attr=None):
+	def hl(self, *args, **kwargs):
 		'''Highlight a segment.
 
 		Returns the default ShellRenderer escape sequence with %{...%} wrapped
 		around it (required in zsh prompts).
 		'''
-		return '%{' + super(ZshPromptRenderer, self).hl(fg, bg, attr) + '%}'
+		return '%{' + super(ZshPromptRenderer, self).hl(*args, **kwargs) + '%}'
+
+	@staticmethod
+	def escape(string):
+		return string.replace('%', '%%')
