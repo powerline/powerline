f3a9e09f3088caec360cc8b2d027ade145e99697
ZyX <kp-pav@ya.ru>
2013-01-26 01:19:48 +0400

    Added support for older ipython versions

--- powerline/bindings/ipython/__init__.py
+++ powerline/bindings/ipython/__init__.py
@@ -59,7 +59,15 @@ try:
 			return setups[key].renderer.render(color=color)
 
 except ImportError:
-	pass
+	from IPython.Prompts import BasePrompt
+	class PowerlinePrompt(BasePrompt):
+		def __init__(self, powerline, *args, **kwargs):
+			self.powerline = powerline
+			super(PowerlinePrompt, self).__init__(*args, **kwargs)
+
+		def set_p_str(self):
+			self.p_str = self.powerline.renderer.render()
+			self.p_str_nocolor = self.powerline.renderer.render(color=False)
 
 def setup(ip=None, prompt_type='in', level=1):
 	if not ip:
@@ -102,5 +110,15 @@ def setup(ip=None, prompt_type='in', level=1):
 	set_prompt(ip, prompt, key)
 	if hasattr(ip, 'prompt_manager'):
 		ip.prompt_manager = PowerlinePromptManager(shell=ip.prompt_manager.shell, config=ip.prompt_manager.config)
+	else:
+		if prompt_type == 'in':
+			attr = 'prompt{0}'.format(level)
+		else:
+			attr = 'prompt_out'
+		def late_startup_hook():
+			old_prompt = getattr(ip.IP.outputcache, attr)
+			setattr(ip.IP.outputcache, attr, PowerlinePrompt(powerline,
+													old_prompt.cache, old_prompt.sep, '', old_prompt.pad_left))
+		ip.IP.hooks.late_startup_hook.add(late_startup_hook)
 
 load_ipython_extension = setup
