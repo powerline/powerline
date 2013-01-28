29793259e10a2815099cd2c81eca6c7d5108588a
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-01-28 17:29:02 +0100

    Improve argument handling in powerline script
    Modules can now be any string, and an informative error message will be
    written to sys.stdout if the module doesn't exist. The
    `last_pipe_status` argument will also automatically be split into
    a list.

--- powerline/segments/shell.py
+++ powerline/segments/shell.py
@@ -7,10 +7,9 @@ last_status.requires_powerline_segment_info = True
 
 
 def last_pipe_status(segment_info):
-	pipe_status = [int(status) for status in segment_info.last_pipe_status.split()]
-	if any(pipe_status):
+	if any(segment_info.last_pipe_status):
 		return [{"contents": str(status), "highlight_group": "exit_fail" if status else "exit_success"}
-			for status in pipe_status]
+			for status in segment_info.last_pipe_status]
 	else:
 		return None
 last_pipe_status.requires_powerline_segment_info = True
