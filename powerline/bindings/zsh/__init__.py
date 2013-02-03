a469dec058e7b6c3730c5e98015b5632a30ee95a
ZyX <kp-pav@ya.ru>
2013-02-03 20:38:07 +0400

    Made zsh bindings restore prompt
    Prompt is restored when zsh/zpython module is unloaded: as ZPYTHON_* variable is 
    readonly (unless __call__ method is defined) it is currently impossible to 
    delete it without unloading module, though prompt can be restored independent of 
    variable deletion. This zsh/zpython behavior is currently considered to be 
    a feature. If __call__ method is defined prompt will also be restored when 
    variable is unset

--- powerline/bindings/zsh/__init__.py
+++ powerline/bindings/zsh/__init__.py
@@ -12,18 +12,28 @@ class Args(object):
 		return zsh.pipestatus()
 
 class Prompt(object):
-	__slots__ = ('render', 'side')
-	def __init__(self, powerline, side):
+	__slots__ = ('render', 'side', 'savedpsvar', 'savedps')
+	def __init__(self, powerline, side, savedpsvar=None, savedps=None):
 		self.render = powerline.renderer.render
 		self.side = side
+		self.savedpsvar = savedpsvar
+		self.savedps = savedps
 
 	def __str__(self):
 		return self.render(width=zsh.columns(), side=self.side).encode('utf-8')
 
+	def __del__(self):
+		if self.savedps:
+			zsh.setvalue(self.savedpsvar, self.savedps)
+
+def set_prompt(powerline, psvar, side):
+	savedps = zsh.getvalue(psvar)
+	zpyvar = 'ZPYTHON_POWERLINE_'+psvar
+	prompt = Prompt(powerline, side, psvar, savedps)
+	zsh.set_special_string(zpyvar, prompt)
+	zsh.setvalue(psvar, '${'+zpyvar+'}')
+
 def setup():
 	powerline = Powerline(ext='shell', renderer_module='zsh_prompt', segment_info=Args())
-
-	zsh.set_special_string('ZPYTHON_PS1', Prompt(powerline, 'left'))
-	zsh.set_special_string('ZPYTHON_RPS1', Prompt(powerline, 'right'))
-	zsh.setvalue('PS1',  '${ZPYTHON_PS1}')
-	zsh.setvalue('RPS1', '${ZPYTHON_RPS1}')
+	set_prompt(powerline, 'PS1', 'left')
+	set_prompt(powerline, 'RPS1', 'right')
