2e5b9383a5ade97697db7a9ed4d782b785456e07
ZyX <kp-pav@ya.ru>
2013-02-06 08:34:35 +0400

    Properly escape color codes in bash
    Closes #177.
    Closes #179.

---
+++ powerline/renderers/bash_prompt.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from powerline.renderers.shell import ShellRenderer
+
+
+class BashPromptRenderer(ShellRenderer):
+	'''Powerline bash prompt segment renderer.'''
+	def hl(self, contents=None, fg=None, bg=None, attr=None):
+		'''Highlight a segment.
+
+		Returns the default ShellRenderer escape sequence with \[...\] wrapped
+		around it (required in bash prompts).
+		'''
+		return '\[' + super(BashPromptRenderer, self).hl(None, fg, bg, attr) + '\]' + (contents or u'')
+
+	@staticmethod
+	def escape(string):
+		return string.replace('\\', '\\\\').replace('$', '\\$').replace('`', '\\`')
