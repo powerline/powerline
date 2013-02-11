95902c99990ca38aa43c396bb109fab48edb0d71
Will Mernagh <wmernagh@gmail.com>
2013-02-12 00:55:13 +0100

    Allow branch_stutus to just update highlight color
    - Renamed branch_with_status to branch_status
    - It now takes the variable `with_branch_name` defaulted to `True`
    - If `with_branch_name` is true it returns the branch name in `contents`

--- powerline/segments/shell.py
+++ powerline/segments/shell.py
@@ -17,17 +17,18 @@ def last_pipe_status(segment_info):
 		return None
 last_pipe_status.requires_powerline_segment_info = True
 
-def branch_with_status():
+def branch_status(with_branch_name=True):
 	'''Return the branch for the current repo highlighted based on status.'''
 	from powerline.lib.vcs import guess
 	repo = guess(os.path.abspath(os.getcwd()))
 	if repo:
 		ret = []
 		branch = repo.branch()
+		contents = branch if with_branch_name else ''
 		status = 'CURRENT' if not repo.status().strip(' ') else 'MODIFIED'
 		ret.append({
-			'contents': branch,
-			'highlight_group': ['branch_with_status_' + status, 'branch_with_status_MODIFIED'],
+			'contents': contents,
+			'highlight_group': ['branch_status_' + status, 'branch_status_MODIFIED'],
 			})
 		return ret
 	return None
