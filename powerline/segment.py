186ad2d423677de0d95cc9a1346c3b671d94e053
ZyX <kp-pav@ya.ru>
2013-02-18 08:31:00 +0400

    Remove useless Segment class
    It was used solely for Segment.get (the only place where it was used just saved
    its .get() method, throwing away instance itself) and looks more like abusal of
    the purpose of the classes

--- powerline/segment.py
+++ powerline/segment.py
@@ -4,35 +4,44 @@ from importlib import import_module
 import sys
 
 
-class Segment(object):
-	def __init__(self, ext, path, default_module=None):
-		self.default_module = default_module or 'powerline.segments.{0}'.format(ext)
-		self.path = path
+def get_function(data, segment):
+	oldpath = sys.path
+	sys.path = data['path'] + sys.path
+	segment_module = str(segment.get('module', data['default_module']))
+	try:
+		return None, getattr(import_module(segment_module), segment['name']), '{0}.{1}'.format(segment_module, segment['name'])
+	finally:
+		sys.path = oldpath
 
-	def get_function(self, segment):
-		oldpath = sys.path
-		sys.path = self.path + sys.path
-		segment_module = str(segment.get('module', self.default_module))
-		try:
-			return None, getattr(import_module(segment_module), segment['name']), '{0}.{1}'.format(segment_module, segment['name'])
-		finally:
-			sys.path = oldpath
 
-	@staticmethod
-	def get_string(segment):
-		return segment.get('contents'), None, None
+def get_string(data, segment):
+	return segment.get('contents'), None, None
 
-	@staticmethod
-	def get_filler(segment):
-		return None, None, None
 
-	def get(self, segment, side):
+def get_filler(data, segment):
+	return None, None, None
+
+
+segment_getters = {
+		"function": get_function,
+		"string": get_string,
+		"filler": get_filler,
+		}
+
+
+def gen_segment_getter(ext, path, default_module=None):
+	data = {
+			"default_module": default_module or 'powerline.segments.'+ext,
+			"path": path
+			}
+
+	def get(segment, side):
 		segment_type = segment.get('type', 'function')
 		try:
-			get_segment_info = getattr(self, 'get_{0}'.format(segment_type))
-		except AttributeError:
+			get_segment_info = segment_getters[segment_type]
+		except KeyError:
 			raise TypeError('Unknown segment type: {0}'.format(segment_type))
-		contents, contents_func, key = get_segment_info(segment)
+		contents, contents_func, key = get_segment_info(data, segment)
 		highlight_group = segment.get('highlight_group', segment.get('name'))
 		divider_highlight_group = segment.get('divider_highlight_group')
 		return {
@@ -58,3 +67,5 @@ class Segment(object):
 			'_space_left': 0,
 			'_space_right': 0,
 			}
+
+	return get
