fdcd7fab23ba515965d2d30f339f8a02d65f369f
Kovid Goyal <kovid@kovidgoyal.net>
2013-03-05 18:05:45 +0530

    Install bzr before running tests

--- tests/install.sh
+++ tests/install.sh
@@ -2,7 +2,7 @@
 pip install .
 if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 	# Python 2
-	pip install mercurial
+	pip install mercurial bzr
 	if python -c 'import sys; sys.exit(1 * (sys.version_info[1] >= 7))' ; then
 		# Python 2.6
 		pip install unittest2
