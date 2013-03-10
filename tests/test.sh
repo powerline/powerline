2fea731c3a26f11139d0583720408d7b5dc18528
ZyX <kp-pav@ya.ru>
2013-03-10 15:13:23 +0400

    Added python_test, more exclusions
    Fixes #289

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-if python -c 'import sys; sys.exit(1 * (sys.version_info >= (2, 7)))' ; then
+: ${PYTHON:=python}
+if ${PYTHON} -c 'import sys; sys.exit(1 * (sys.version_info >= (2, 7)))' ; then
 	# Python 2.6
 	export PYTHONPATH="${PYTHONPATH}:`realpath .`"
 	for file in tests/test_*.py ; do
-		if ! python $file ; then
+		if ! ${PYTHON} $file ; then
 			exit 1
 		fi
 	done
 else
-	python setup.py test
+	${PYTHON} setup.py test
 fi
