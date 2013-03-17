84dfac6f8e06823255d33180be82c7fa08dea135
ZyX <kp-pav@ya.ru>
2013-03-17 13:55:31 +0400

    Avoid using python setup.py test
    Closes #295

--- tests/test.sh
+++ tests/test.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 : ${PYTHON:=python}
 FAILED=0
-if ${PYTHON} -c 'import sys; sys.exit(1 * (sys.version_info >= (2, 7)))' ; then
-	# Python 2.6
-	export PYTHONPATH="${PYTHONPATH}:`realpath .`"
-	for file in tests/test_*.py ; do
-		if ! ${PYTHON} $file ; then
-			FAILED=1
-		fi
-	done
-else
-	if ! ${PYTHON} setup.py test ; then
+export PYTHONPATH="${PYTHONPATH}:`realpath .`"
+for file in tests/test_*.py ; do
+	if ! ${PYTHON} $file ; then
 		FAILED=1
 	fi
-fi
+done
 if ! ${PYTHON} scripts/powerline-lint ; then
 	FAILED=1
 fi
