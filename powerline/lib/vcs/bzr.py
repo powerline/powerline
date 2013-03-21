ec40de3180d3402aa137b4f36498b50b5afbcc07
Kovid Goyal <kovid@kovidgoyal.net>
2013-03-22 00:24:07 +0530

    Reduce overhead when fetching branch name for bzr
    Since the move to threaded vcs segments, repo.branch() is called very
    often, currently every 0.2 seconds. This leads to lots of overhead, as
    it means that every 0.2 seconds the Branch object has to be created, and
    the various config files read and parsed, just to get the branch name.
    And all this happens in python while holding the GIL.
    
    Changed that to simply extract the branch name from the relevant config
    file directly. If in the future you decide to merge my file_watcher.py
    module into powerline, I can refactor this to remove even that overhead
    by watching the relevant config file. I suggest doing something similar
    for the git and mercurial plugins as well, but that is upto whoever
    maintains them.

--- powerline/lib/vcs/bzr.py
+++ powerline/lib/vcs/bzr.py
@@ -2,9 +2,11 @@
 from __future__ import absolute_import, unicode_literals, division, print_function
 
 import sys
+import os
+import re
 from io import StringIO
 
-from bzrlib import (branch, workingtree, status, library_state, trace, ui)
+from bzrlib import (workingtree, status, library_state, trace, ui)
 
 
 class CoerceIO(StringIO):
@@ -13,13 +15,13 @@ class CoerceIO(StringIO):
 			arg = arg.decode('utf-8', 'replace')
 		return super(CoerceIO, self).write(arg)
 
+state = None
 
 class Repository(object):
 	def __init__(self, directory):
 		if isinstance(directory, bytes):
 			directory = directory.decode(sys.getfilesystemencoding() or sys.getdefaultencoding() or 'utf-8')
 		self.directory = directory
-		self.state = library_state.BzrLibraryState(ui=ui.SilentUIFactory, trace=trace.DefaultConfig())
 
 	def status(self, path=None):
 		'''Return status of repository or file.
@@ -39,6 +41,9 @@ class Repository(object):
 			pass
 
 	def _status(self, path):
+		global state
+		if state is None:
+			state = library_state.BzrLibraryState(ui=ui.SilentUIFactory, trace=trace.DefaultConfig())
 		buf = CoerceIO()
 		w = workingtree.WorkingTree.open(self.directory)
 		status.show_tree_status(w, specific_files=[path] if path else None, to_file=buf, short=True)
@@ -57,8 +62,16 @@ class Repository(object):
 		return ans if ans.strip() else None
 
 	def branch(self):
+		ans = None
 		try:
-			b = branch.Branch.open(self.directory)
-			return b._get_nick(local=True) or None
-		except:
+			pat = re.compile(br'nickname\s*=\s*(.+)')
+			with open(os.path.join(self.directory, '.bzr', 'branch', 'branch.conf'), 'rb') as f:
+				for line in f:
+					m = pat.match(line)
+					if m is not None:
+						ans = m.group(1).strip()
+						break
+		except Exception:
 			pass
+		return ans or os.path.basename(self.directory)
+
