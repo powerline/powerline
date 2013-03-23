83517f65b98f0c8cfa943e42a08c181ca81a1c9b
ZyX <kp-pav@ya.ru>
2013-03-24 01:42:50 +0400

    Make test.sh always check only powerline/config_files directory

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,7 @@ for file in tests/test_*.py ; do
 		FAILED=1
 	fi
 done
-if ! ${PYTHON} scripts/powerline-lint ; then
+if ! ${PYTHON} scripts/powerline-lint -p powerline/config_files ; then
 	FAILED=1
 fi
 exit $FAILED
