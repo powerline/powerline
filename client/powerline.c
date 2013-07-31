a122e73b9b64a16a929330bd21cf1bcf9e206546
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-07-31 22:15:35 +0200

    Add files from @kovidgoyal's powerline-daemon repo
    Minor changes have been applied:
    
    - Removed copyright info and GPL 3 license since Powerline is MIT (needs
      confirmation from kovidgoyal before merge)
    - The `powerline-client` script is renamed to `powerline`, and calls the
      daemon or `powerline-render` (the previous `powerline`) to render
      a statusline
    - Minor coding style corrections to match the rest of the project
    - Python 3 support is removed for now due to setuptools failing with
      binary scripts
    
    Todo:
    
    - Automatically attempt to launch powerline-daemon the first time
      powerline is run if the daemon isn't already running?
    - pip install -e fails with binary files (it appears that pip recodes
      the powerline binary to ASCII, the compiled powerline script must be
      copied manually to ~/.local/bin after pip install -e has been run)

---
+++ client/powerline.c
@@ -0,0 +1,115 @@
+/* vim:fileencoding=utf-8:noet */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define handle_error(msg) \
+	do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression) \
+  (                                                                           \
+	({ long int __result;                                                     \
+	   do __result = (long int) (expression);                                 \
+	   while (__result == -1L && errno == EINTR);                             \
+	   __result; }))
+#endif
+
+extern char **environ;
+
+void do_write(int sd, const char *raw, int len) {
+	int written = 0, n = -1;
+
+	while (written < len) {
+		n = TEMP_FAILURE_RETRY(write(sd, raw+written, len-written));
+		if (n == -1) {
+			close(sd);
+			handle_error("write() failed");
+		}
+		written += n;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	int sd = -1, i;
+	struct sockaddr_un server;
+	char address[50] = {};
+	const char eof[2] = "\0\0";
+	char buf[4096] = {};
+	char *newargv[200] = {};
+	char *wd = NULL;
+	char **envp;
+
+	if (argc < 2) { printf("Must provide at least one argument.\n"); return EXIT_FAILURE; }
+
+#ifdef __APPLE__
+	snprintf(address, 50, "/tmp/powerline-ipc-%d", getuid());
+#else
+	snprintf(address, 50, "powerline-ipc-%d", getuid());
+#endif
+
+	sd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sd == -1) handle_error("socket() failed");
+
+	memset(&server, 0, sizeof(struct sockaddr_un)); // Clear 
+	server.sun_family = AF_UNIX;
+#ifdef __APPLE__
+	strncpy(server.sun_path, address, strlen(address));
+#else
+	strncpy(server.sun_path+1, address, strlen(address));
+#endif
+
+#ifdef __APPLE__
+	if (connect(sd, (struct sockaddr *) &server, sizeof(server.sun_family) + strlen(address)) < 0) {
+#else
+	if (connect(sd, (struct sockaddr *) &server, sizeof(server.sun_family) + strlen(address)+1) < 0) {
+#endif
+		close(sd);
+		// We failed to connect to the daemon, execute powerline instead
+		argc = (argc < 199) ? argc : 199;
+		for (i=1; i < argc; i++) newargv[i] = argv[i];
+		newargv[0] = "powerline-render";
+		newargv[argc] = NULL;
+		execvp("powerline-render", newargv);
+	}
+
+	for (i = 1; i < argc; i++) {
+		do_write(sd, argv[i], strlen(argv[i]));
+		do_write(sd, eof, 1);
+	}
+
+	for(envp=environ; *envp; envp++) {
+		do_write(sd, "--env=", 6);
+		do_write(sd, *envp, strlen(*envp));
+		do_write(sd, eof, 1);
+	}
+
+	wd = getcwd(NULL, 0);
+	if (wd != NULL) {
+		do_write(sd, "--cwd=", 6);
+		do_write(sd, wd, strlen(wd));
+		free(wd); wd = NULL;
+	}
+
+	do_write(sd, eof, 2);
+
+	i = -1;
+	while (i != 0) {
+		i = TEMP_FAILURE_RETRY(read(sd, buf, 4096));
+		if (i == -1) {
+			close(sd);
+			handle_error("read() failed");
+		}
+		if (i > 0) 
+			write(STDOUT_FILENO, buf, i) || 0;
+	}
+
+	close(sd);
+
+	return 0;
+}
--- setup.py
+++ setup.py
@@ -3,16 +3,42 @@
 from __future__ import unicode_literals
 import os
 import sys
+import subprocess
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 try:
-	README = open(os.path.join(here, 'README.rst'), 'rb').read().decode('utf-8')
+	README = open(os.path.join(CURRENT_DIR, 'README.rst'), 'rb').read().decode('utf-8')
 except IOError:
 	README = ''
 
-old_python = sys.version_info < (2, 7)
+OLD_PYTHON = sys.version_info < (2, 7)
+
+
+def compile_client():
+	'''Compile the C powerline-client script.'''
+
+	if hasattr(sys, 'getwindowsversion'):
+		raise NotImplementedError()
+	if sys.version_info >= (3, 0):
+		# FIXME Python 3 doesn't allow compiled C files to be included in the 
+		# scripts list below. This is because Python 3 distutils tries to 
+		# decode the file to ASCII, and fails when powerline-client is 
+		# a binary.
+		raise NotImplementedError()
+	else:
+		from distutils.ccompiler import new_compiler
+		compiler = new_compiler().compiler
+		subprocess.check_call(compiler + ['-O3', 'client/powerline.c', '-o', 'scripts/powerline'])
+
+try:
+	compile_client()
+except Exception:
+	# FIXME Catch more specific exceptions
+	import shutil
+	print('Compiling C version of powerline-client failed, using Python version instead')
+	shutil.copyfile('client/powerline.py', 'scripts/powerline')
 
 setup(
 	name='Powerline',
@@ -26,6 +52,8 @@ setup(
 	scripts=[
 		'scripts/powerline',
 		'scripts/powerline-lint',
+		'scripts/powerline-daemon',
+		'scripts/powerline-render',
 		'scripts/powerline-config',
 	],
 	keywords='',
@@ -38,5 +66,5 @@ setup(
 			'Sphinx',
 		],
 	},
-	test_suite='tests' if not old_python else None,
+	test_suite='tests' if not OLD_PYTHON else None,
 )
