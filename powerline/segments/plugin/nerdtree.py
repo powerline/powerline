2e38ab568651cec323d76c9748859b2b3bfdc2ad
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-08-20 13:41:53 +0200

    Add NERDTree statusline

---
+++ powerline/segments/plugin/nerdtree.py
@@ -0,0 +1,17 @@
+# vim:fileencoding=utf-8:noet
+
+import vim
+
+from powerline.segments.vim import window_cached
+
+
+@window_cached
+def nerdtree(pl):
+	ntr = vim.eval('getbufvar("%", "NERDTreeRoot")')
+	if not ntr:
+		return
+	path_str = vim.eval('getbufvar("%", "NERDTreeRoot").path.str()')
+	return [{
+		'contents': path_str,
+		'highlight_group': ['nerdtree.path', 'file_name'],
+	}]
