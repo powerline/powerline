d1793fac61ea2ded98b76a10da563f1a53950b06
ZyX <kp-pav@ya.ru>
2014-01-13 21:48:09 +0400

    Run apt-get with sudo and -qq like suggested in travis documentation
    Documentation though suggests using before_install. Not sure why.

--- tests/install.sh
+++ tests/install.sh
@@ -9,5 +9,5 @@ if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 		pip install unittest2 argparse
 	fi
 fi
-apt-get install zsh screen
+sudo apt-get install -qq zsh screen
 true
