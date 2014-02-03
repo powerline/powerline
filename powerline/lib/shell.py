44083581511a76e04fb417d1f71775871fa6f8cf
ZyX <kp-pav@ya.ru>
2014-02-03 21:20:53 +0400

    Move NowPlaying._run_cmd to powerline.lib.shell.run_cmd, make asrun use it

---
+++ powerline/lib/shell.py
@@ -0,0 +1,26 @@
+# vim:fileencoding=utf-8:noet
+
+from subprocess import Popen, PIPE
+
+
+def run_cmd(pl, cmd, stdin=None):
+	try:
+		p = Popen(cmd, stdout=PIPE, stdin=PIPE)
+	except OSError as e:
+		pl.exception('Could not execute command ({0}): {1}'.format(e, cmd))
+		return None
+	else:
+		stdout, err = p.communicate(stdin)
+	return stdout.strip()
+
+
+def asrun(pl, ascript):
+	'''Run the given AppleScript and return the standard output and error.'''
+	return run_cmd(pl, ['osascript', '-'], ascript)
+
+
+def asquote(astr):
+	'''Return the AppleScript equivalent of the given string.'''
+
+	astr = astr.replace('"', '" & quote & "')
+	return '"{}"'.format(astr)
