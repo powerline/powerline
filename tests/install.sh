773d6a6600d111a40471492c4e0955a6eb56713c
ZyX <kp-pav@ya.ru>
2014-02-09 01:25:18 +0400

    Also use --allow-unverified

--- tests/install.sh
+++ tests/install.sh
@@ -4,7 +4,7 @@ pip install psutil
 if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 	# Python 2
 	pip install mercurial
-	pip install --allow-external bzr bzr
+	pip install --allow-external bzr --allow-unverified bzr bzr
 	if python -c 'import sys; sys.exit(1 * (sys.version_info[1] >= 7))' ; then
 		# Python 2.6
 		pip install unittest2 argparse
