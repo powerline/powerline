7d89ff2b493cd403954a35da8201712a2f3beb9c
S0lll0s <s0lll0s@blinkenshell.org>
2014-02-09 11:10:49 +0100

    Fix docstring for i3wm.workspaces segment

--- powerline/segments/i3wm.py
+++ powerline/segments/i3wm.py
@@ -7,12 +7,12 @@ def calcgrp( w ):
 	group = ["workspace"]
 	if w['urgent']: group.append( 'w_urgent' )
 	if w['visible']: group.append( 'w_visible' )
-	if w['focused']: return "w_focused" #group.append( 'w_focused' )
+	if w['focused']: return "w_focused"
 	return group
 
 def workspaces( pl ):
 	'''Return workspace list
 
-	Highlight groups used: ``workspace, visible, focused, urgent``
+	Highlight groups used: ``workspace``, ``w_visible``, ``w_focused``, ``w_urgent``
 	'''
 	return [ {'contents': w['name'], 'highlight_group': calcgrp( w )} for w in i3.get_workspaces() ]
