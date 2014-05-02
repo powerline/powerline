d78efb0a317e01a9f0a5fda69301ff4e45736a4f
Ivan Malison <IvanMalison@gmail.com>
2014-05-02 16:18:18 -0700

    Refactor tmux renderer.

--- powerline/renderers/tmux.py
+++ powerline/renderers/tmux.py
@@ -15,47 +15,40 @@ class TmuxRenderer(Renderer):
 	def hlstyle(self, fg=None, bg=None, attr=None):
 		'''Highlight a segment.'''
 		# We don't need to explicitly reset attributes, so skip those calls
-		if not attr and not bg and not fg:
-			return ''
-		tmux_attr = []
-		if fg is not None:
-			if fg is False or fg[0] is False:
-				tmux_attr += ['fg=default']
-			else:
-				tmux_attr += ['fg=colour' + str(fg[0])]
-		if bg is not None:
-			if bg is False or bg[0] is False:
-				tmux_attr += ['bg=default']
-			else:
-				tmux_attr += ['bg=colour' + str(bg[0])]
-		if attr is not None:
-			if attr is False:
-				tmux_attr += ['nobold', 'noitalics', 'nounderscore']
-			else:
-				if attr & ATTR_BOLD:
-					tmux_attr += ['bold']
-				else:
-					tmux_attr += ['nobold']
-				if attr & ATTR_ITALIC:
-					tmux_attr += ['italics']
-				else:
-					tmux_attr += ['noitalics']
-				if attr & ATTR_UNDERLINE:
-					tmux_attr += ['underscore']
-				else:
-					tmux_attr += ['nounderscore']
-		return '#[' + ','.join(tmux_attr) + ']'
+		if not any((attr, bg, fg)): return ''
+		return '#[' + ','.join(self._get_tmux_attributes(fg, bg, attr)) + ']'
 
 	def get_segment_info(self, segment_info, mode):
-		r = self.segment_info.copy()
+		new_segment_info = self.segment_info.copy()
 		if segment_info:
-			r.update(segment_info)
-		if 'pane_id' in r:
-			varname = 'TMUX_PWD_' + r['pane_id'].lstrip('%')
-			if varname in r['environ']:
-				r['getcwd'] = lambda: r['environ'][varname]
-		r['mode'] = mode
-		return r
+			new_segment_info.update(segment_info)
+		if 'pane_id' in new_segment_info:
+			self._add_get_cwd_to_segment_info(new_segment_info)
+		new_segment_info['mode'] = mode
+		return new_segment_info
+
+	def _add_get_cwd_to_segment_info(self, segment_info):
+		varname = 'TMUX_PWD_' + segment_info['pane_id'].lstrip('%')
+		if varname in segment_info['environ']:
+			segment_info['getcwd'] = lambda: segment_info['environ'][varname]
+
+	@staticmethod
+	def _get_color_string(color, color_type):
+		if color is False or color[0] is False:
+			return '{0}=default'.format(color_type)
+		else:
+			return '{color_type}=colour{color}'.format(color_type=color_type,
+													   color=color[0])
+
+	def _get_tmux_attributes(self, fg=None, bg=None, attr=None):
+		if fg is not None:
+			yield self._get_color_string(fg)
+			yield self._get_color_string(bg)
+		if attr is not None:
+			attr = attr or 0 # Converts False to 0
+			yield 'bold' if attr & ATTR_BOLD else 'nobold'
+			yield 'italics' if attr & ATTR_ITALIC else 'noitalics'
+			yield 'underscore' if attr & ATTR_UNDERLINE else 'nounderscore'
 
 
 renderer = TmuxRenderer
