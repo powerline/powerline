623395a0130299c5bfcecd5505be3855c8a764ee
ZyX <kp-pav@yandex.ru>
2014-06-28 22:18:45 +0400

    Remove outdated comment

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -513,8 +513,6 @@ class TestVCS(TestCase):
 			call(['git', 'branch', 'branch1'], cwd=GIT_REPO)
 			call(['git', 'checkout', '-q', 'branch1'], cwd=GIT_REPO)
 			self.do_branch_rename_test(repo, 'branch1')
-			# For some reason the rest of this test fails on travis and only on
-			# travis, and I can't figure out why
 			call(['git', 'branch', 'branch2'], cwd=GIT_REPO)
 			call(['git', 'checkout', '-q', 'branch2'], cwd=GIT_REPO)
 			self.do_branch_rename_test(repo, 'branch2')
