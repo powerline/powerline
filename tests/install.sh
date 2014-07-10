19195159adc5ec0df35fc8cb7064c6f2f0b570f5
ZyX <kp-pav@yandex.ru>
2014-07-10 19:29:56 +0400

    Only install mercurial and bazaar if using CPython

--- tests/install.sh
+++ tests/install.sh
@@ -3,8 +3,10 @@ pip install .
 pip install psutil
 if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 	# Python 2
-	pip install mercurial
-	pip install --allow-external bzr --allow-unverified bzr bzr
+	if python -c 'import platform, sys; sys.exit(1 - (platform.python_implementation() == "CPython"))' ; then
+		pip install mercurial
+		pip install --allow-external bzr --allow-unverified bzr bzr
+	fi
 	if python -c 'import sys; sys.exit(1 * (sys.version_info[1] >= 7))' ; then
 		# Python 2.6
 		pip install unittest2 argparse
