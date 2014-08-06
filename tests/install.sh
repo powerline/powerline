3e43995d2ce2dd5a757daae02bdd6dda9f559e93
ZyX <kp-pav@yandex.ru>
2014-08-06 17:21:01 +0400

    Use pip to install ipython, not apt-get
    I am very unsure that it will install ipython for all required python versions

--- tests/install.sh
+++ tests/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install .
-pip install psutil
+pip install psutil ipython
 if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 	# Python 2
 	if python -c 'import platform, sys; sys.exit(1 - (platform.python_implementation() == "CPython"))' ; then
@@ -12,7 +12,7 @@ if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 		pip install unittest2 argparse
 	fi
 fi
-sudo apt-get install -qq screen zsh tcsh mksh busybox ipython
+sudo apt-get install -qq screen zsh tcsh mksh busybox
 # Travis has too outdated fish. It cannot be used for tests.
 # sudo apt-get install fish
 true
