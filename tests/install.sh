accb174b8ba855556b00b0f5ff4ae1f207928ba7
ZyX <kp-pav@yandex.ru>
2014-08-06 18:06:54 +0400

    Do not install ipython when using python-2.6

--- tests/install.sh
+++ tests/install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 pip install .
-pip install psutil ipython
+pip install psutil
 if python -c 'import sys; sys.exit(1 * (sys.version_info[0] != 2))' ; then
 	# Python 2
 	if python -c 'import platform, sys; sys.exit(1 - (platform.python_implementation() == "CPython"))' ; then
+		# PyPy
 		pip install mercurial
 		pip install --allow-external bzr --allow-unverified bzr bzr
 	fi
 	if python -c 'import sys; sys.exit(1 * (sys.version_info[1] >= 7))' ; then
 		# Python 2.6
 		pip install unittest2 argparse
+	else
+		# Python 2.7
+		pip install ipython
 	fi
 fi
 sudo apt-get install -qq screen zsh tcsh mksh busybox
