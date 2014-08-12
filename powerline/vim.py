da867b26a9a98f19119987d26433a6524dbd52e5
ZyX <kp-pav@yandex.ru>
2014-08-12 20:33:03 +0400

    Fix VimPowerline.add_local_theme
    After #783 it started to fail as it made Theme require defining dividers in 
    theme and add_local_theme did not merge in other themes (__main__ and top).

--- powerline/vim.py
+++ powerline/vim.py
@@ -51,8 +51,17 @@ class VimPowerline(Powerline):
 		'''
 		self.update_renderer()
 		key = self.get_matcher(key)
+		theme_config = {}
+		for cfg_path in self.theme_levels:
+			try:
+				lvl_config = self.load_config(cfg_path, 'theme')
+			except IOError:
+				pass
+			else:
+				mergedicts(theme_config, lvl_config)
+		mergedicts(theme_config, config)
 		try:
-			self.renderer.add_local_theme(key, {'config': config})
+			self.renderer.add_local_theme(key, {'config': theme_config})
 		except KeyError:
 			return False
 		else:
