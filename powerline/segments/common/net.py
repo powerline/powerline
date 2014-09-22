05cb1d2d3ce33135a60d3b927ad50149dd62e050
ZyX <kp-pav@yandex.ru>
2014-09-22 07:42:42 +0400

    Lower the priority of various VM bridge interfaces
    Closes #1094

--- powerline/segments/common/net.py
+++ powerline/segments/common/net.py
@@ -72,13 +72,16 @@ except ImportError:
 		return None
 else:
 	_interface_starts = {
-		'eth':   10,  # Regular ethernet adapters         : eth1
-		'enp':   10,  # Regular ethernet adapters, Gentoo : enp2s0
-		'ath':    9,  # Atheros WiFi adapters             : ath0
-		'wlan':   9,  # Other WiFi adapters               : wlan1
-		'wlp':    9,  # Other WiFi adapters, Gentoo       : wlp5s0
-		'teredo': 1,  # miredo interface                  : teredo
-		'lo':   -10,  # Loopback interface                : lo
+		'eth':      10,  # Regular ethernet adapters         : eth1
+		'enp':      10,  # Regular ethernet adapters, Gentoo : enp2s0
+		'ath':       9,  # Atheros WiFi adapters             : ath0
+		'wlan':      9,  # Other WiFi adapters               : wlan1
+		'wlp':       9,  # Other WiFi adapters, Gentoo       : wlp5s0
+		'teredo':    1,  # miredo interface                  : teredo
+		'lo':      -10,  # Loopback interface                : lo
+		'docker':   -5,  # Docker bridge interface           : docker0
+		'vmnet':    -5,  # VMWare bridge interface           : vmnet1
+		'vboxnet':  -5,  # VirtualBox bridge interface       : vboxnet0
 	}
 
 	_interface_start_re = re.compile(r'^([a-z]+?)(\d|$)')
