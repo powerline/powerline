3f2aabb77bf9c7e79eace61c3da048271c97b17a
ZyX <kp-pav@yandex.ru>
2014-09-26 00:20:58 +0400

    Refactor Vim and common branch segments to share code

--- powerline/segments/common/vcs.py
+++ powerline/segments/common/vcs.py
@@ -2,32 +2,45 @@
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
 from powerline.lib.vcs import guess, tree_status
+from powerline.segments import Segment, with_docstring
 from powerline.theme import requires_segment_info, requires_filesystem_watcher
 
 
 @requires_filesystem_watcher
 @requires_segment_info
-def branch(pl, segment_info, create_watcher, status_colors=False):
-	'''Return the current VCS branch.
+class BranchSegment(Segment):
+	divider_highlight_group = None
 
-	:param bool status_colors:
-		determines whether repository status will be used to determine highlighting. Default: False.
+	@staticmethod
+	def get_directory(segment_info):
+		return segment_info['getcwd']()
 
-	Highlight groups used: ``branch_clean``, ``branch_dirty``, ``branch``.
-	'''
-	name = segment_info['getcwd']()
-	repo = guess(path=name, create_watcher=create_watcher)
-	if repo is not None:
-		branch = repo.branch()
-		scol = ['branch']
-		if status_colors:
-			try:
-				status = tree_status(repo, pl)
-			except Exception as e:
-				pl.exception('Failed to compute tree status: {0}', str(e))
-				status = '?'
-			scol.insert(0, 'branch_dirty' if status and status.strip() else 'branch_clean')
-		return [{
-			'contents': branch,
-			'highlight_group': scol,
-		}]
+	def __call__(self, pl, segment_info, create_watcher, status_colors=False):
+		name = self.get_directory(segment_info)
+		if name:
+			repo = guess(path=name, create_watcher=create_watcher)
+			if repo is not None:
+				branch = repo.branch()
+				scol = ['branch']
+				if status_colors:
+					try:
+						status = tree_status(repo, pl)
+					except Exception as e:
+						pl.exception('Failed to compute tree status: {0}', str(e))
+						status = '?'
+					scol.insert(0, 'branch_dirty' if status and status.strip() else 'branch_clean')
+				return [{
+					'contents': branch,
+					'highlight_group': scol,
+					'divider_highlight_group': self.divider_highlight_group,
+				}]
+
+
+branch = with_docstring(BranchSegment(),
+'''Return the current VCS branch.
+
+:param bool status_colors:
+	determines whether repository status will be used to determine highlighting. Default: False.
+
+Highlight groups used: ``branch_clean``, ``branch_dirty``, ``branch``.
+''')
