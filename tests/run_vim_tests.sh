5d6caaeecd285c14bcffd17c26172de4a859a46c
ZyX <kp-pav@yandex.ru>
2014-12-09 19:20:46 +0300

    Use compiled Vim binaries for Vim tests
    Part 1: use latest single-python Vim builds

--- tests/run_vim_tests.sh
+++ tests/run_vim_tests.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+. tests/bot-ci/scripts/common/main.sh
+VIM=$PWD/tests/bot-ci/deps/vim/tip-$PYTHON_VERSION/vim
 FAILED=0
-for script in tests/*.vim ; do
-	if ! vim -u NONE -S $script || test -f message.fail ; then
-		echo "Failed script $script" >&2
+for script in tests/test_*.vim ; do
+	if ! $VIM -u NONE -S $script || test -f message.fail ; then
+		echo "Failed script $script run with $VIM" >&2
 		cat message.fail >&2
 		rm message.fail
 		FAILED=1
