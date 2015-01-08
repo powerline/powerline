b00d7ee35624b2ffb53c3a448cfc07ebe9cf2d05
ZyX <kp-pav@yandex.ru>
2015-01-08 17:08:25 +0300

    Prettify --quiet daemon argument definition

--- powerline/commands/daemon.py
+++ powerline/commands/daemon.py
@@ -6,7 +6,15 @@ import argparse
 
 def get_argparser(ArgumentParser=argparse.ArgumentParser):
 	parser = ArgumentParser(description='Daemon that improves powerline performance.')
-	parser.add_argument('--quiet', '-q', action='store_true', help='Without other options: do not complain about already running powerline-daemon instance. Will still exit with 1. With `--kill\' and `--replace\': do not show any messages. With `--foreground\': ignored. Does not silence exceptions in any case.')
+	parser.add_argument(
+		'--quiet', '-q', action='store_true',
+		help='Without other options: do not complain about already running '
+		     'powerline-daemon instance. '
+		     'Will still exit with 1. '
+		     'With `--kill\' and `--replace\': do not show any messages. '
+		     'With `--foreground\': ignored. '
+		     'Does not silence exceptions in any case.'
+	)
 	parser.add_argument('--socket', '-s', help='Specify socket which will be used for connecting to daemon.')
 	arggr = parser.add_mutually_exclusive_group().add_argument
 	arggr('--kill', '-k', action='store_true', help='Kill an already running instance.')
