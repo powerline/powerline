8fff21d77b331e78ef6683ba19c7040441cdd5c2
ZyX <kp-pav@yandex.ru>
2015-02-20 22:36:39 +0300

    Set the default value for POWERLINE_TMUX_EXE

--- tests/test_in_vterm/test.sh
+++ tests/test_in_vterm/test.sh
@@ -38,8 +38,10 @@ test_tmux() {
 if test -z "$POWERLINE_TMUX_EXE" && test -d tests/bot-ci/deps/tmux ; then
 	for tmux in tests/bot-ci/deps/tmux/tmux-*/tmux ; do
 		export POWERLINE_TMUX_EXE="$PWD/$tmux"
+		test_tmux || true
 	done
 else
+	export POWERLINE_TMUX_EXE="${POWERLINE_TMUX_EXE:-tmux}"
 	test_tmux || true
 fi
 
