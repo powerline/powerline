fb0fba6c6d176b2cd82f89e5f8bf52412e5aeacb
ZyX <kp-pav@yandex.ru>
2015-02-21 02:00:13 +0300

    Do not modify $PATH and $LD_LIBRARY_PATH unless on travis

--- tests/test.sh
+++ tests/test.sh
@@ -8,18 +8,20 @@ export USER HOME
 
 FAILED=0
 
-export PATH="/opt/fish/bin:${PATH}"
+if test "$TRAVIS" = true ; then
+	export PATH="/opt/fish/bin:${PATH}"
 
-if test "$PYTHON_IMPLEMENTATION" = "CPython" ; then
-	export PATH="/opt/zsh-${PYTHON_MM}${USE_UCS2_PYTHON:+-ucs2}/bin:${PATH}"
-fi
+	if test "$PYTHON_IMPLEMENTATION" = "CPython" ; then
+		export PATH="/opt/zsh-${PYTHON_MM}${USE_UCS2_PYTHON:+-ucs2}/bin:${PATH}"
+	fi
 
-if test -n "$USE_UCS2_PYTHON" ; then
-	export LD_LIBRARY_PATH="/opt/cpython-ucs2-$UCS2_PYTHON_VARIANT/lib${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
-	set +e
-	. virtualenvwrapper.sh
-	workon cpython-ucs2-$UCS2_PYTHON_VARIANT
-	set -e
+	if test -n "$USE_UCS2_PYTHON" ; then
+		export LD_LIBRARY_PATH="/opt/cpython-ucs2-$UCS2_PYTHON_VARIANT/lib${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
+		set +e
+		. virtualenvwrapper.sh
+		workon cpython-ucs2-$UCS2_PYTHON_VARIANT
+		set -e
+	fi
 fi
 
 export PYTHON="${PYTHON:=python}"
