4a3798b43b494e894429b524ab09ba784900c157
Foo <kp-pav@yandex.ru>
2015-03-01 17:23:07 +0300

    Fix tcsh renderer: end prompt with a sequence that is not dropped

--- powerline/renderers/shell/tcsh.py
+++ powerline/renderers/shell/tcsh.py
@@ -12,5 +12,20 @@ class TcshPromptRenderer(ZshPromptRenderer):
 	character_translations[ord('^')] = '\\^'
 	character_translations[ord('!')] = '\\!'
 
+	def do_render(self, **kwargs):
+		ret = super(TcshPromptRenderer, self).do_render(**kwargs)
+		nbsp = self.character_translations.get(ord(' '), ' ')
+		end = self.hlstyle()
+		assert not ret or ret.endswith(end)
+		if ret.endswith(nbsp + end):
+			# Exchange nbsp and highlight end because tcsh removes trailing 
+			# %{%} part of the prompt for whatever reason
+			ret = ret[:-(len(nbsp) + len(end))] + end + nbsp
+		else:
+			# We *must* end prompt with non-%{%} sequence for the reasons 
+			# explained above. So add nbsp if it is not already there.
+			ret += nbsp
+		return ret
+
 
 renderer = TcshPromptRenderer
