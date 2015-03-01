619c289e21243859d7611799bce871b61db9b3e6
Foo <kp-pav@yandex.ru>
2015-03-01 18:15:24 +0300

    Check in the -config wrapper whether any signals were received

--- tests/test_in_vterm/powerline-config-wrapper.sh
+++ tests/test_in_vterm/powerline-config-wrapper.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+for signal in ABRT ALRM BUS CHLD CONT FPE HUP ILL INT PIPE QUIT SEGV TERM TSTP \
+              TTIN TTOU USR1 USR2 PROF SYS TRAP URG VTALRM XCPU XFSZ ; do
+	trap "echo Got signal $signal >> signal.log" $signal
+done
 real-powerline-config "$@" > powerline-config.out.log 2>&1
