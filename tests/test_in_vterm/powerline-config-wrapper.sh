e0915fa1a4c4c18ddbd1f462ed07b77f12c4c43c
Foo <kp-pav@yandex.ru>
2015-04-02 22:48:17 +0300

    Add shell PID to log

--- tests/test_in_vterm/powerline-config-wrapper.sh
+++ tests/test_in_vterm/powerline-config-wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 for signal in ABRT ALRM BUS CHLD CONT FPE HUP ILL INT PIPE QUIT SEGV TERM TSTP \
               TTIN TTOU USR1 USR2 PROF SYS TRAP URG VTALRM XCPU XFSZ ; do
-	trap "echo Got signal $signal >> signal.log" $signal
+	trap "echo $$: Got signal $signal >> signal.log" $signal
 done
 real-powerline-config "$@" > powerline-config.out.log 2>&1
 exitcode=$?
-echo Returned $exitcode >> returncode.log
+echo $$: Returned $exitcode >> returncode.log
 exit $exitcode
