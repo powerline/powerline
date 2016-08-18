23b82d922c5b727870a2e6b456688d18aebcc482
pozar <lpozarlik@gmail.com>
2016-08-18 21:54:28 +0200

    small fix of issue with restarting powerline process for awesome window manager

--- powerline/bindings/awesome/powerline.lua
+++ powerline/bindings/awesome/powerline.lua
@@ -8,4 +8,4 @@ function powerline(mode, widget) end
 
 bindings_path = string.gsub(debug.getinfo(1).source:match('@(.*)$'), '/[^/]+$', '')
 powerline_cmd = bindings_path .. '/powerline-awesome.py'
-awful.util.spawn_with_shell('ps -C powerline-awesome.py || ' .. powerline_cmd)
+awful.util.spawn_with_shell('ps aux | grep -e powerline-awesome.py | grep -v grep || ' .. powerline_cmd)
