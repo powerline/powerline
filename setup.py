234091c95dd6e99dd272367dfe0a42316b29ea73
Foo <kp-pav@yandex.ru>
2017-07-01 02:57:53 +0300

    Specify argparse dependency
    Fixes #1791

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
 	packages=find_packages(exclude=('tests', 'tests.*')),
 	include_package_data=True,
 	zip_safe=False,
-	install_requires=[],
+	install_requires=['argparse'] if OLD_PYTHON else [],
 	extras_require={
 		'docs': [
 			'Sphinx',
