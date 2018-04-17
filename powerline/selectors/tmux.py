123a5c80652d3db62cfa25f21f52f1478c050e1c
ruben <r.simons@fz-juelich.de>
2018-04-17 08:13:56 +0200

    Refactored tmux.py to be more beautiful now (This breaks working status though)

--- powerline/selectors/tmux.py
+++ powerline/selectors/tmux.py
@@ -1,32 +1,23 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import (unicode_literals, division, absolute_import, print_function)
-
 from powerline.bindings.tmux import get_tmux_output
-import subprocess
-import re
+
+# import psutil
 
 _cached_clients = {}
-# This Regex parses pstree -s <Process_ID> information
-# It will match, if any process name is sshd
-SSHD_REGEX = re.compile(r"(?:^|\W)sshd(?:^|\W)")
 
 
-def tmux_via_ssh(pl, segment_info, mode, caching=True, default=False):
-	'''Returns True if tmux client is connected via SSH
-	'''
-	pid = get_tmux_output(pl, "display", "-p", "#{client_pid}")
-	if not pid.isnumeric():
-		return default
-	if caching:
-		if pid in _cached_clients:
-			return _cached_clients[pid]
+def tmux_via_ssh(pl, segment_info, mode):
+	'''Returns True if tmux client is connected via SSH'''
+	current_process = psutil.Process()
 	try:
-		pstree_out = subprocess.check_output(("pstree", "-s", pid)).decode()
-	except subprocess.CalledProcessError:
-		return default
-	# Will be True if a Matcher gets returned ('sshd' is found), 
-	# False if None is returned
-	is_ssh = bool(SSHD_REGEX.search(pstree_out))
-	if caching:
-		_cached_clients[pid] = is_ssh
-	return is_ssh
+		return _cached_clients[current_process.pid]
+	except KeyError:
+		# Iterate through parents
+		while current_process is not None and current_process.pid != 1:
+			if current_process.name() == "sshd":
+				_cached_clients[current_process.pid] = True
+				return True
+			current_process = current_process.parent()
+		_cached_clients[current_process.pid] = False
+		return False
