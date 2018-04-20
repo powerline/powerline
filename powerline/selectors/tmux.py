1c928df49caeaed810c1b2c0de4f81e879e5a874
ruben <r.simons@fz-juelich.de>
2018-04-20 09:56:21 +0200

    Improved Docstring Added Error Handling for psutil not being installed

--- powerline/selectors/tmux.py
+++ powerline/selectors/tmux.py
@@ -2,15 +2,23 @@
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 from powerline.bindings.tmux import get_tmux_output
 
-import psutil
 
 _cached_clients = {}
 
-
 def tmux_via_ssh(pl, segment_info, mode):
-	'''Returns True if tmux client is connected via SSH'''
+	'''Returns True if tmux client is connected via SSH
+	
+	Requires the ``psutil`` module.
+	
+	Works only for tmux versions >= 2.1'''
+	try:
+		import psutil
+	except ImportError:
+		pl.warn('Module "psutil" is not installed, thus we cannot detect whether tmux is run via ssh')
+		return False
 	process_id = segment_info['args'].process_id
 	if process_id is None:
+		pl.warn("Process ID was not passed, maybe you are using tmux version < 2.1")
 		return False # or True
 	current_process = psutil.Process(segment_info['args'].process_id)
 	try:
@@ -18,7 +26,7 @@ def tmux_via_ssh(pl, segment_info, mode):
 	except KeyError:
 		# Iterate through parents
 		while current_process is not None and current_process.pid != 1:
-			if current_process.name() == "sshd":
+			if current_process.name() == 'sshd':
 				_cached_clients[current_process.pid] = True
 				return True
 			current_process = current_process.parent()
