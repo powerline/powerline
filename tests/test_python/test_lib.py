1135fbdba23c5fe07127fc3fd4a24e0ad8910ea0
Ruben Simons <ruben.simons@web.de>
2020-10-11 16:43:49 +0200

    The duration / 0.2 should be greater, but don't take too much of a look at the performance. This is to test on machines with high load, especially GitHub Actions

--- tests/test_python/test_lib.py
+++ tests/test_python/test_lib.py
@@ -236,7 +236,7 @@ class TestThreaded(TestCase):
 			('update', 'def'),
 		])
 		num_runs = len([e for e in log if e[0] == 'update'])
-		self.assertAlmostEqual(duration / 0.2, num_runs, delta=1)
+		assert duration / 0.2 >= num_runs
 		log[:] = ()
 
 		segment = TestSegment()
@@ -266,7 +266,7 @@ class TestThreaded(TestCase):
 			('update', 'ghi'),
 		])
 		num_runs = len([e for e in log if e[0] == 'update'])
-		self.assertAlmostEqual(duration / 0.2, num_runs, delta=1)
+		assert duration / 0.2 >= num_runs
 		self.assertEqual(num_runs - 1, len(pl.exceptions))
 		log[:] = ()
 
