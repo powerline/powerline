39135728deb303a21a4478bb377bf10fac49b30c
ruben <ruben.simons@web.de>
2020-10-14 00:33:55 +0200

    Set env["LANG"] to make awesome tests reproducable

--- tests/test_awesome/conftest.py
+++ tests/test_awesome/conftest.py
@@ -57,6 +57,7 @@ def daemon_process(xprocess, daemon, daemon_env):
 def daemon_env():
     env = os.environ.copy()
     env["XDG_CONFIG_HOME"] = os.path.abspath(os.path.dirname(__file__))
+    env["LANG"] = "C"
     env["PATH"] = "{}:{}".format(os.path.join(os.path.abspath(os.path.dirname(__file__)), "path"), env["PATH"])
     return env
 
